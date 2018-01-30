7f05d2d37fc984dafce0fb6fe29a4b6e5e8a4400
silvestrevivo <silvestrevivomillan@gmail.com>
2018-01-30 11:20:14 +0100

    html, sass and javascript setup

--- index.js
+++ index.js
@@ -6,4 +6,30 @@
 // import scss to bundle it with parcel
 import './style.scss';
 
-console.log('budgetjs with Parcel');
+// import modules
+// we create an scope with internal and no accesible data from each other
+// and as well with publics methods which could be called from outside
+// this is possible thanks to closures in JavaScript
+
+// module1 - BUDGET CONTROLLER
+var budgetControler = (function() {
+    // this is private
+    // this is public
+})();
+
+// module 2 - UI CONTROLLER
+var UIController = (function() {
+    //..
+})();
+
+// module 3 - GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+    //internal scope
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        console.log('Buttom was click');
+    });
+
+    // accesible public
+})(budgetControler, UIController);
+
+console.log(controller.anotherPublic());
