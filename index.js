2e2e14dd52adab7e3f322db04adbe3d04830a452
silvestrevivo <silvestrevivomillan@gmail.com>
2018-01-30 08:14:57 +0100

    babel package added for ES6

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// This project is an example of plain JavaScript application
+// The compilation in development and production is done with ParcelJS
+// Just start npm init to add the configuration modules and that's all
+// For this case are imported : node-sass and babel-preset-env
+
 // import scss to bundle it with parcel
 import './style.scss';
 
