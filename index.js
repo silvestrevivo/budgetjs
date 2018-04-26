8fa59e512a93a5358a6c38cf5d71719fe882f6e1
silvestrevivo <silvestrevivomillan@gmail.com>
2018-04-26 15:56:33 +0200

    cleaning up fields after input and focus added

--- index.js
+++ index.js
@@ -92,26 +92,41 @@ var UIController = (function () {
         },
 
         addListItem: function (obj, type) {
-            var html, newHtml, element;
+            var html, newHtml, element, fieldsArr;
             // Create HTML string with placeholder text
 
             if (type === 'inc') {
                 element = DOMstrings.incomeContainer
                 html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
-            } else if (type === 'inc') {
+            } else if (type === 'exp') {
                 element = DOMstrings.expensesContainer
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             }
 
             // Replace the placeholder text with some actual data
-            newHtml = html.replace('%id%', obj.id)
-            newHtml = newHtml.replace('%description%', obj.description)
-            newHtml = newHtml.replace('%value%', obj.value)
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
 
             // Insert the HTML into the DOM
-            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
         },
+
+        clearFields: function () {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            // what we get is an Array and we have to convert it in something editable in the DOM
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function (current, index, array) {
+                current.value = '';
+            })
+
+            fieldsArr[0].focus();
+        },
+
         getDOMstring: function (params) {
             return DOMstrings;
         }
@@ -150,7 +165,9 @@ var controller = (function (budgetCtrl, UICtrl) {
         console.log('input', input);
         console.log('newItem', newItem);
         // 3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type)
+        UICtrl.addListItem(newItem, input.type);
+        // Clear the fields
+        UICtrl.clearFields();
 
     };
 
